8b9e54e8b105043ee673bf8cf4a243a17f1d3219
Reginald Johnson <reginald.johnson@gmail.com>
2016-06-13 13:31:37 -0400

    first commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+    public float ballInitialVelocity = 600;
+
+    private Rigidbody rb;
+    private bool ballInPlay = false;
+
+	void Awake () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if ((Input.GetButtonDown("Fire1")) && (!ballInPlay))
+        {
+            transform.parent = null;
+            ballInPlay = true;
+            rb.isKinematic = false;
+            rb.AddForce(new Vector3(ballInitialVelocity, ballInitialVelocity, 0));
+        }
+	}
+}
